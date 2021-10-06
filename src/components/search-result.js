0cbf1dd2f8c2246a2bcaec453f29fa28b5fb9f08
NicolasLlanos8799 <nicollanos8799@gmail.com>
2021-10-06 14:57:34 -0300

    Subiendo nuevas cosas

--- src/components/search-result.js
+++ src/components/search-result.js
@@ -1,35 +1,61 @@
 import React, { Component } from "react";
 import ArtistCard from "./artist-card.js";
+import Loading from "./loading.js";
 
 class SearchResult extends Component {
   state = {
-    data: "",
+    loading: false,
+    error: null,
+    data: {
+      similarartists: {
+        artist: [],
+      },
+    },
   };
 
   componentDidMount() {
     this.fetchData(
-      "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=Metallica&api_key=77093375607eafd9a343d126e20d8eee&format=json"
+      "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=Intoxicados&api_key=77093375607eafd9a343d126e20d8eee&format=json"
     );
   }
 
   fetchData = async (url) => {
+    this.setState({
+      loading: true,
+    });
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({
-      data: data,
-    });
+    console.log(data, "lo q trae la api");
+
+    if (data.error) {
+      this.setState({
+        loading: false,
+        error: true,
+      });
+    } else {
+      this.setState({
+        loading: false,
+        data: data,
+      });
+    }
   };
 
   render() {
     return (
       <React.Fragment>
+        {this.state.loading && <Loading></Loading>}
+        {this.state.error && <h1>Error de algo</h1>}
         <div className="container">
           <div className="row">
-            {/* {this.state.artist.map((artista, i) => {
+            {this.state.data.similarartists.artist.map((item, i) => {
               return (
-                <ArtistCard img={artista.image} titulo={artista.name} key={i} />
+                <ArtistCard
+                  img={item.image[4]["#text"]}
+                  titulo={item.name}
+                  key={i}
+                />
               );
-            })} */}
+            })}
 
             <h1>{this.props.busqueda}</h1>
           </div>
